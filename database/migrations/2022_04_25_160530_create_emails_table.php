d960cc374c8120f3c87e86a512c50dbd83913b8c
DDEV-Local User <nobody@example.com>
2022-06-06 10:39:56 +0000

    creating avatar

--- database/migrations/2022_04_25_160530_create_emails_table.php
+++ database/migrations/2022_04_25_160530_create_emails_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('emails', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique();
-            $table->string('avatar') -> nullable();
             $table->timestamps();
         });
     }
