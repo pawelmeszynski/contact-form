fbd7be3256ac94d68d39a8c1c4fccfb8ff06f3b4
Paweł Meszyński <p.meszynski99@gmail.com>
2022-06-09 20:35:58 +0200

    Sanctum working correctly

--- app/Http/Controllers/Api/AuthApiController.php
+++ app/Http/Controllers/Api/AuthApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Hash;
 use Illuminate\Http\Request;
 use Auth;
 
@@ -25,40 +26,16 @@ class AuthApiController extends Controller
         $request->validate([
             'name' => 'required|min:3',
             'email' => 'required|email|unique:users',
-            'password' => 'required|min:4|confirmed'
+            'password' => 'required|min:4'
         ]);
 
         $user = User::create([
             'name' => ucwords($request->name),
             'email' => $request->email,
-            'password' => bcrypt($request->password),
+            'password' => Hash::make(($request->password)),
         ]);
 
         return $this->response($user);
     }
 
-    public function login(Request $request)
-    {
-        $cred = $request->validate([
-            'email' => 'required|email|exists:users',
-            'password' => 'required|min:4',
-        ]);
-
-        if ( !Auth::attempt($cred)) {
-            return response()->json([
-                'message' => 'Unauthorized'
-            ], 401);
-        }
-
-        return $this->response(Auth::user());
-    }
-
-    public function logout()
-    {
-        Auth::user()->tokens()->delete();
-
-        return response()->json([
-            'message' => 'You have successfully logged out and token was successfully deleted.'
-        ]);
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,30 +7,26 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
-
 Route::post('register', [AuthApiController::class, 'register']);
-Route::post('login', [AuthApiController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function() {
-    Route::post('logout', [AuthApiController::class, 'logout']);
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', function (Request $request) {
         return $request->user();
     });
+
+    Route::apiresource('emails', EmailsController::class)->except('show');
+
+    Route::get('/emails', [EmailsController::class, 'index']);
+
+    Route::get('/emails/{id}', [EmailsController::class, 'show']);
+
+    Route::put('/emails/{id}/update', [EmailsController::class, 'updateById']);
+
+    Route::post('/emails/create', [EmailsController::class, 'create']);
+
+    Route::get('/trash', [TrashController::class, 'index']);
+
+    Route::get('/emails/{id}/restore', [TrashController::class, 'restore'])->name('trash.restore');
+
+    Route::delete('emails/{id}', [TrashController::class, 'destroy'])->name('trash.destroy');
 });
-
-Route::apiresource('emails', EmailsController::class)->except('show');
-
-Route::get('/emails', [EmailsController::class, 'index']);
-
-Route::get('/emails/{id}', [EmailsController::class, 'show']);
-
-Route::put('/emails/{id}/update', [EmailsController::class, 'updateById']);
-
-Route::post('/emails/create', [EmailsController::class, 'create']);
-
-Route::get('/trash', [TrashController::class, 'index']);
-
-Route::get('/emails/{id}/restore', [TrashController::class, 'restore'])->name('trash.restore');
-
-Route::delete('emails/{id}', [TrashController::class, 'destroy'])->name('trash.destroy');
-
